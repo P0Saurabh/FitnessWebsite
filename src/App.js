bfa1ffc3aa96f0cd35714753a3c7c00cd51adb4a
P0Saurabh <saurabhsp0402@gmail.com>
2024-05-28 22:37:58 +0530

    chart
    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import * as React from 'react';
+import {  ThemeProvider, createTheme } from '@mui/material';
+import ChartsOverviewDemo from './Card';  // Make sure the path to your component is correct
 
-import './App.css';
+// Create a theme instance
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: '#ff1744',
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
 
 function App() {
   return (
-    <h1>hello    </h1>
-    
+    <ThemeProvider theme={theme}>
+      <h1>hi</h1>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+        <ChartsOverviewDemo />
+      </div>
+    </ThemeProvider>
   );
 }
 
